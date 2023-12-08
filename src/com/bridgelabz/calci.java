137a0e8e36c5182771200e33b8663b929ede7f95
pa1zore <pavanzore345@gmail.com>
2023-12-08 11:31:26 +0530

    Added: addition  of two number

--- src/com/bridgelabz/calci.java
+++ src/com/bridgelabz/calci.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class calci {
     public static void main(String[] args) {
         System.out.println("welcome to calculator");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two number  for addition"); //printing message on console to
+        int num1=sc.nextInt();//reading number from console
+        int num2=sc.nextInt();//reading second number from console
+        //performing addition here
+        int sum=num1+num2;
+        System.out.println("addition of two numbers is "+sum);
     }
 }
